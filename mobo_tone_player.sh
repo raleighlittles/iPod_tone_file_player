95a4a404ef558b2e72df6d39317c88058641c937
Raleigh Littles III <raleighlittles@gmail.com>
2024-01-14 21:17:52 -0800

    shell only: dont hardcode the beep string

--- mobo_tone_player.sh
+++ mobo_tone_player.sh
@@ -2,7 +2,7 @@
 
 
 set -e
-set -x
+set +x
 
 ## Takes a given tone file and plays it using `beep`.
 ## Requires a motherboard speaker!
@@ -22,22 +22,25 @@ else
 
     # echo "Reading line $tone_cmd"
 
-    if test "$tone_cmd" = "Beep"
+    # There's no official specification for Tone files but it seems like basically the first line is for metadata
+
+    if [[ $tone_cmd =~ ^[[:digit:]] ]];
     then
-        echo "Skipping header line!"
+        tone_cmd_arr=( $tone_cmd )
+
+        ## Debug
+        # echo "Tone command array: $tone_cmd_arr has length ${#tone_cmd_arr[@]}"
+        # echo "Beep frequency (Hz) ${tone_cmd_arr[0]}"
+        # echo "Beep duration (ms)  ${tone_cmd_arr[1]}"
+
+        beep -f ${tone_cmd_arr[0]} -l ${tone_cmd_arr[1]}
+
+    else
+        echo "Skipping line $tone_cmd"
         continue
     fi
 
-    tone_cmd_arr=( $tone_cmd )
 
-    ## Debug
-    # echo "Tone command array: $tone_cmd_arr has length ${#tone_cmd_arr[@]}"
-    # echo "Beep frequency (Hz) ${tone_cmd_arr[0]}"
-    # echo "Beep duration (ms)  ${tone_cmd_arr[1]}"
-
-    beep -f ${tone_cmd_arr[0]} -l ${tone_cmd_arr[1]}
-
-    done < $tone_file
-
+    done < "$tone_file"
 
 fi
